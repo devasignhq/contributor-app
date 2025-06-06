5a655dbb77d5c898a463783c1bbcc3b85ee67847
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-06-06 02:06:02 +0100

    Initialize app

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Geist_Mono } from "next/font/google";
+import "./globals.css";
+import ToastProvider from "./components/ToastProvider";
+
+const geistMono = Geist_Mono({
+    subsets: ["latin"],
+    variable: "--font-geist-mono",
+});
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <head>
+                <title>DevAsign</title>
+                <meta name="description" content="The Open Source Project Management Tool" />
+            </head>
+            <body className={`${geistMono.className} antialiased`}>
+                <ToastProvider>
+                    {children}
+                </ToastProvider>
+            </body>
+        </html>
+    );
+}
