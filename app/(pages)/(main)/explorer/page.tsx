7c6a43a14c1b58e09022fe1a1c74866ce35a0f46
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-07-29 14:40:01 +0100

    Made explorer and settings page a 'coming soon' feature

--- app/(pages)/(main)/explorer/page.tsx
+++ app/(pages)/(main)/explorer/page.tsx
@@ -2,7 +2,7 @@
 
 const Explorer = () => {
     return (
-        <div className="w-[80%] mx-auto">
+        <div className="w-[80%] mx- auto">
             <section className="w-full space-y-5 mt-[30px] mb-[50px]">
                 <h1 className="text-display-small text-light-100">Coming Soon...</h1>
             </section>
